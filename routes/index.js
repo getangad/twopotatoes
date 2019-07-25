12cd48dbdf3d0d8447c9ac940c6359be7acb0d85
Mahesh <mahesh.av@questionpro.com>
2019-07-26 01:46:40 +0530

    made changes to create game room and join the existing rooms

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Two Potatoes' });
+  //res.render('index', { title: 'Two Potatoes' });
+
+    res.render('home');
 });
 
 module.exports = router;
