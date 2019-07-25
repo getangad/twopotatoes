5ce545c9fe08af74fb949e62ac43f082ec8e1927
Mahesh <mahesh.av@questionpro.com>
2019-07-26 01:43:41 +0530

    made changes to create game room and join the existing rooms

--- gameSocket.js
+++ gameSocket.js
@@ -3,56 +3,135 @@ var gameIO;
 const GAME_NAMESPACE = "/game";
 const MAXIMUM_PLAYERS_ALLOWED = 2;
 
+
 const SocketEvents = {
-  CONNECTION: 'connection',
-  DISCONNECT: 'disconnect'
+    CONNECTION: 'connection',
+    DISCONNECT: 'disconnect'
 }
 
+const roomNameList = [];
+const roomObjects = [];
+
 module.exports = function (socketio) {
-  io = socketio;
-  attach(socketio);
+    io = socketio;
+    attach(socketio);
 }
 
 function isTheRoomFull(roomno, maximumPlayersAllowedInOneRoom) {
-  return io.nsps[GAME_NAMESPACE].adapter.rooms[getRoomName(roomno)]
-      && io.nsps[GAME_NAMESPACE].adapter.rooms[getRoomName(roomno)].length
-      > (maximumPlayersAllowedInOneRoom - 1);
+    return io.nsps[GAME_NAMESPACE].adapter.rooms[getRoomName(roomno)]
+        && io.nsps[GAME_NAMESPACE].adapter.rooms[getRoomName(roomno)].length
+        > (maximumPlayersAllowedInOneRoom - 1);
+}
+
+function getNumberOfPlayersInRoom(roomPin){
+    return io.nsps[GAME_NAMESPACE].adapter.rooms[getRoomName(roomPin)] ?
+        io.nsps[GAME_NAMESPACE].adapter.rooms[getRoomName(roomPin)].length : 0
 }
 
 function attach() {
-  var roomno = 1;
-  gameIO = io.of(GAME_NAMESPACE);
-  gameIO.on(SocketEvents.CONNECTION, function (socket) {
-    console.log('A user connected');
-    if (isTheRoomFull(roomno, MAXIMUM_PLAYERS_ALLOWED)) {
-      roomno++;
-    }
-    socket.join(getRoomName(roomno));
+    let roomno = 1;
+    gameIO = io.of(GAME_NAMESPACE);
 
-    emitToGameRoom(ClientHandledEvents.ROOM_JOINED,
-        "You are in room no. " + roomno, roomno)
 
-    Object.keys(ServerHandledEvents).forEach((value) => {
-      socket.on(value, (data) => ServerHandledEvents[value](socket, data));
+    gameIO.on(SocketEvents.CONNECTION, function (socket) {
+        /* console.log('A user connected');
+         if (isTheRoomFull(roomno, MAXIMUM_PLAYERS_ALLOWED)) {
+           roomno++;
+         }
+         socket.join(getRoomName(roomno));
+
+         emitToGameRoom(ClientHandledEvents.ROOM_JOINED,
+             "You are in room no. " + roomno, roomno)
+
+         Object.keys(ServerHandledEvents).forEach((value) => {
+           socket.on(value, (data) => ServerHandledEvents[value](socket, data));
+         });
+
+         socket.on(SocketEvents.DISCONNECT, function () {
+           console.log('A user disconnected', socket.rooms);
+         });*/
+
+        console.log("whats up");
+        Object.keys(ServerHandledEvents).forEach((value) => {
+            socket.on(value, (data) => ServerHandledEvents[value](socket, data));
+        });
+
+        socket.on(SocketEvents.DISCONNECT, function () {
+            console.log('A user disconnected', socket.id);
+            roomObjects.filter(roomObject => {
+                let i = 0;
+                for (; i < roomObject.players.length; i++) {
+                    let item = roomObject.players[i];
+                    let players = roomObject.players;
+                    if (item && item.socketID == socket.id) {
+                        roomObject.players = [
+                            ...players.slice(0, i),
+                            ...players.slice(i + 1)
+                        ]
+                        break;
+                    }
+                }
+            })
+        });
+
     });
-
-    socket.on(SocketEvents.DISCONNECT, function () {
-      console.log('A user disconnected', socket.rooms);
-    });
-  });
 }
 
 function getRoomName(roomNo) {
-  return "room-" + roomNo;
+    return "room-" + roomNo;
 }
 
-function emitToGameRoom(clientHandledEvent, message, roomNumber) {
-  if (roomNumber) {
-    gameIO.to(getRoomName(roomNumber)).emit(clientHandledEvent, message);
-  } else {
+function emitToGameRoom(clientHandledEvent, message, roomID) {
+    if (roomID && roomNameList.includes(getRoomName(roomID))) {
+        gameIO.to(getRoomName(roomID)).emit(clientHandledEvent, message);
+    } else {
+        gameIO.emit(clientHandledEvent, message);
+    }
+}
 
-    gameIO.emit(clientHandledEvent, message);
-  }
+function createRoomAndAddSocket(socket, data) {
+    let roomName = getRoomName(data.roomPin);
+    let roomObject = {
+        "name": roomName,
+        "players": [
+            {
+                "socketID": socket.id,
+                "name": data.name,
+                "team": data.team
+            }
+        ]
+    };
+    roomNameList.push(roomName);
+    socket.join(roomName);
+    roomObjects.push(roomObject);
+    console.log("room object");
+    console.log(roomObjects);
+}
+
+
+
+function joinRoomIfExists(socket, data) {
+    let roomName = getRoomName(data.roomPin);
+    console.log("join room if exists "+ roomName);
+    console.log(roomNameList)
+    if(roomNameList.includes(roomName)){
+        socket.join(roomName);
+        getRoomObject(roomName).players.push({
+            "socketID": socket.id,
+            "name": data.name,
+            "team": data.team
+        });
+        console.log(data.name + " joined room "+ roomName);
+        console.log(getRoomObject(roomName).players);
+    }else{
+        console.log("Invalid Pin !!!!! do something about it ################");
+    }
+}
+
+function getRoomObject(roomName) {
+    console.log(roomName);
+    console.log(roomObjects)
+    return roomObjects.filter(roomObject => roomName == roomObject.name)[0];
 }
 
 /*  ---------------------------------------------------------------
@@ -61,19 +140,42 @@ function emitToGameRoom(clientHandledEvent, message, roomNumber) {
  */
 
 const ServerHandledEvents = {
-  START: startGame,
-  PLAYER_DIED: playerDied
+    START: startGame,
+    PLAYER_DIED: playerDied,
+    CREATE_ROOM: createRoom,
+    JOIN_ROOM: joinRoom
 }
 
 function startGame(socket, data) {
-  console.log("start received:", data);
+    console.log("start received:", data);
 }
 
 
 function playerDied(socket, data) {
-  console.log("yeah", data);
+    console.log("yeah", data);
+    emitToGameRoom(ClientHandledEvents.ROOM_JOINED, {"ndansj": "asf"}, 1);
+}
 
-  emitToGameRoom(ClientHandledEvents.ROOM_JOINED, {"ndansj":"asf"}, 1);
+function createRoom(socket, data) {
+    console.log("inside create room " + data.roomPin);
+    createRoomAndAddSocket(socket, data);
+    emitToGameRoom(ClientHandledEvents.ROOM_CREATED,
+        {
+            "message": "You have created room " + data.roomPin,
+            numberOfPlayers: 1
+        }
+        , data.roomPin)
+}
+
+
+function joinRoom(socket, data) {
+    console.log("inside join room " + data.roomPin);
+    joinRoomIfExists(socket, data);
+    console.log("player joined "+ getNumberOfPlayersInRoom(data.roomPin));
+    emitToGameRoom(ClientHandledEvents.ROOM_JOINED, {
+        roomObject: getRoomObject(getRoomName(data.roomPin)),
+        numberOfPlayers: getNumberOfPlayersInRoom(data.roomPin)
+    })
 }
 
 /*  ---------------------------------------------------------------
@@ -84,6 +186,7 @@ function playerDied(socket, data) {
     ---------------------------------------------------------------
  */
 const ClientHandledEvents = {
-  ROOM_JOINED: "ROOM_JOINED"
+    ROOM_JOINED: "ROOM_JOINED",
+    ROOM_CREATED: "ROOM_CREATED"
 }
 
--- public/javascripts/websocket-client.js
+++ public/javascripts/websocket-client.js
@@ -1,20 +1,76 @@
 const ServerHandledEvents = {
   START: "START",
-  PLAYER_DIED: "PLAYER_DIED"
+  PLAYER_DIED: "PLAYER_DIED",
+  CREATE_ROOM: "CREATE_ROOM",
+  JOIN_ROOM: "JOIN_ROOM"
 }
 
 const ClientHandledEvents = {
-  ROOM_JOINED: "ROOM_JOINED"
+  ROOM_JOINED: "ROOM_JOINED",
+  ROOM_CREATED: "ROOM_CREATED"
 }
+
+
+var createRoomBtn = document.getElementById('createRoom'),
+    roomID = document.getElementById('roomID'),
+    playerCount = document.getElementById('playerCount'),
+    joinRoomBtn = document.getElementById('joinRoom'),
+
+    hostNameInput = document.getElementById('hostName'),
+
+    roomPinInput = document.getElementById('roomPin'),
+    playerNameInput = document.getElementById('playerName');
+
 
 var socket = io.connect("/game");
 
-socket.on(ClientHandledEvents.ROOM_JOINED, function (data) {
-  console.log("Room Joined: " + data);
-  socket.emit(ServerHandledEvents.START, 'ohh yeah');
-});
 
 // so that can be utilized by game.js
 var WebsocketClient = {
 
 };
+
+
+//create room
+createRoomBtn.addEventListener('click', function(){
+  console.log("started to create room");
+  let roomPin = getRoomPin();
+  socket.emit(ServerHandledEvents.CREATE_ROOM, {
+    "roomPin": roomPin,
+    "name": hostNameInput.value,
+    "team": document.getElementById('hostTeam').value
+  });
+  roomID.innerHTML += '<p><strong> PIN : ' + roomPin +'</strong></p>';
+});
+
+
+//join room
+joinRoomBtn.addEventListener('click', function(){
+    console.log("started to join room");
+    socket.emit(ServerHandledEvents.JOIN_ROOM, {
+        "roomPin": roomPinInput.value,
+        "name": playerNameInput.value,
+        "team": document.getElementById('playerTeam').value
+    });
+});
+
+
+
+function getRoomPin(){
+  return Math.floor(Math.random()*90000) + 10000;
+}
+
+
+
+
+socket.on(ClientHandledEvents.ROOM_JOINED, function (data) {
+    console.log("Room Joined: ");
+    console.log( data);
+    console.log("players info: " + data.players);
+    playerCount.innerHTML = '<p><strong> number of players : ' + data.numberOfPlayers +'</strong></p>';
+});
+
+socket.on(ClientHandledEvents.ROOM_CREATED, function (data) {
+    console.log("Room created: " + data.message);
+    playerCount.innerHTML = '<p><strong> number of players : ' + data.numberOfPlayers +'</strong></p>';
+});
